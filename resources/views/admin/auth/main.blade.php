67a0ee900debc4736e9fc28cabdbcc8d07a9ed7e
haohexin <2554470320@qq.com>
2017-10-30 15:31:07 +0800

    Updated backend login page

--- resources/views/admin/auth/main.blade.php
+++ resources/views/admin/auth/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Backend</title>
+    <title>后台管理系统</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -117,10 +117,10 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 2.0.0
+            <b>Version</b> 1.0.0
         </div>
         <strong>Copyright &copy; 2016-{{ \Carbon\Carbon::now()->year }} <a
-                    href="#" target="_blank">Backend</a>.
+                    href="#" target="_blank">🐷丽</a>.
         </strong> All rights reserved.
     </footer>
     <!-- Add the sidebar's background. This div must be placed
--- resources/views/admin/auth/showLogin.blade.php
+++ resources/views/admin/auth/showLogin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Backend</title>
+    <title>后台管理系统</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -30,10 +30,10 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>Backend</b></a>
+        <a href="#"><b>Ken&Glee's　Backend</b></a>
     </div>
     <div class="login-logo">
-        <a href="#">V2.0.0</a>
+        <a href="#">V1.0.0</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
